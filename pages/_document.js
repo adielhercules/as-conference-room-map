51a4c36974386d2f15984bc7dc02df6f42191976
Adiel Hercules <jadher.11x2@gmail.com>
2018-11-16 16:58:20 -0600

    Add Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,15 @@ export default class MyDocument extends Document {
             href="https://cdn.pannellum.org/2.3/pannellum.css"
           />
           {this.props.styleTags}
+          {'<!-- Global site tag (gtag.js) - Google Analytics -->'}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129315878-1"></script>
+          {`<script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'UA-129315878-1');
+              </script>`}
+
         </Head>
         <body>
           <Main />
